2a1363fe4f266d28c3e18cd31a7a3a1f3156e8f9
Tahsin Rashad <tahsin.rashad716@gmail.com>
2025-04-05 11:26:04 +0100

    treasure decay implementation fix

--- src/entities/treasure.py
+++ src/entities/treasure.py
@@ -1,12 +1,20 @@
 from src.entities.base_entity import BaseEntity
 
-
 class Treasure(BaseEntity):
     def __init__(self, x, y, t_type):
         super().__init__(x, y)
         self.t_type = t_type
-        self.value = t_type.value
+        self.value = float(t_type.value)
 
     def decay(self):
-        self.value -= 0.1
+        if self.value > 0:
+            decay_amount = self.value * 0.001
+            self.value -= decay_amount
+
+            if self.value <= 0:
+                 self.value = 0
+
         return self.value > 0
+
+    def __repr__(self):
+        return f"Treasure({self.t_type.name}, val={self.value:.2f} at ({self.x},{self.y}))"
