68bafd26c5b08a55431e38018e6236a192f672d6
Tahsin Rashad <tahsin.rashad716@gmail.com>
2025-04-05 11:20:36 +0100

    hideout hunter recruit implementation ongoing

--- src/entities/hideout.py
+++ src/entities/hideout.py
@@ -1,4 +1,5 @@
 from src.entities.base_entity import BaseEntity
+from src.entities.hunter import Hunter
 
 
 class Hideout(BaseEntity):
@@ -6,3 +7,10 @@ class Hideout(BaseEntity):
         super().__init__(x, y)
         self.hunters = []
         self.stored_treasure = []
+
+    def recruit(self, world):
+        if len(self.hunters) < 5 and self.stored_treasure:
+            self.stored_treasure.pop()
+            new_hunter = Hunter(self.x, self.y)
+            self.hunters.append(new_hunter)
+            world.hunters.append(new_hunter)
--- src/entities/hunter.py
+++ src/entities/hunter.py
@@ -11,17 +11,28 @@ class Hunter(BaseEntity):
         self.carrying = None
 
     def act(self, world):
-        self.stamina -= 2
         if self.stamina <= 0:
             return
 
-        for dx in [-1, 0, 1]:
-            for dy in [-1, 0, 1]:
-                tx, ty = (self.x + dx) % GridConfig.GRID_SIZE, (self.y + dy) % GridConfig.GRID_SIZE
-                treasure = world.get_treasure_at(tx, ty)
-                if treasure:
-                    self.carrying = treasure
-                    world.treasures.remove(treasure)
-                    return
+        self.stamina -= 1
+
+        if self.carrying or self.stamina <= 20:
+            nearest_hideout = min(world.hideouts, key=lambda h: abs(h.x - self.x) + abs(h.y - self.y))
+            dx = 1 if nearest_hideout.x > self.x else -1 if nearest_hideout.x < self.x else 0
+            dy = 1 if nearest_hideout.y > self.y else -1 if nearest_hideout.y < self.y else 0
+            self.move(dx, dy)
+
+            if self.x == nearest_hideout.x and self.y == nearest_hideout.y:
+                self.stamina = 100
+                if self.carrying:
+                    nearest_hideout.stored_treasure.append(self.carrying)
+                    self.carrying = None
+            return
+
+        treasure = world.get_treasure_at(self.x, self.y)
+        if treasure:
+            self.carrying = treasure
+            world.treasures.remove(treasure)
+            return
 
         self.move(random.choice([-1, 0, 1]), random.choice([-1, 0, 1]))
